c78ce9eaa32c4b80c03e898e7a6af7924b973f18
Thomas Lowry <minilao94@yahoo.com>
2018-07-28 15:34:40 -0600

    added massive

--- server/app/index.js
+++ server/app/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
 const bodyParser = require('body-parser');
+const massive = require('massive');
 
 // ENVIRONMENT
 require('dotenv').config();
